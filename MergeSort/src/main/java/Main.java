d969c88e415c6cd3192f66bbc78b63394e84e869
Heshan-w <htwanigasinghe@gmail.com>
2024-01-28 23:37:06 +0530

    Merge sort explanation(logic not implemented)

---
+++ MergeSort/src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // merge sort is O(n log n)
+        // it's a divide and conquer algorithm.
+        // it divides the input array into two halves, calls itself for the two halves, and then merges the two sorted halves.
+        // it's a recursive algorithm.
+        // it's a stable sorting algorithm (it preserves the order of equal elements).
+
+        // it's efficient for large lists and for data that is in RAM (not on disk).
+        // it's not efficient for small lists, linked lists.
+
+    }
+}
