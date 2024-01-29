0c3547e17765ab77aa3fa350e12bb4018fbfd66c
Heshan-w <htwanigasinghe@gmail.com>
2024-01-29 22:55:31 +0530

    Merge sort explanation(logic not implemented)

--- MergeSort/src/main/java/Main.java
+++ MergeSort/src/main/java/Main.java
@@ -9,5 +9,27 @@ public class Main {
         // it's efficient for large lists and for data that is in RAM (not on disk).
         // it's not efficient for small lists, linked lists.
 
+        int[] dataList = {23, 54, 28, 6, 86, 22, 55}; // unordered data
+
+        System.out.print("Before Merge sort : "); // Print the data before performing Merge sort
+        for (int num : dataList) {
+            System.out.print(num + " ");
+        }
+
+        // Perform Merge sort by passing the data list
+        performMergeSort(dataList, 0, dataList.length - 1);
+
+        System.out.println("\n");
+        System.out.print("After Merge sort  : "); // Print the data after Merge sort
+        for (int num : dataList) {
+            System.out.print(num + " ");
+        }
+
+
+
+    }
+
+    private static void performMergeSort(int[] dataList, int i, int i1) {
+
     }
 }
